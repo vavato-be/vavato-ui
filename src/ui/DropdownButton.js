fd7d7a875752b79e93269e18e77024a31f6d8714
Alex Reis <alex@alexmreis.com>
2021-07-13 21:15:00 +0200

    chore: Fix prettier

--- src/ui/DropdownButton.js
+++ src/ui/DropdownButton.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components'
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
-import ButtonWithIcon from './ButtonWithIcon';
-import Dropdown from './Dropdown';
+import ButtonWithIcon from './ButtonWithIcon'
+import Dropdown from './Dropdown'
 
 const Container = styled('div')`
   display: flex;
@@ -13,32 +13,34 @@ const Container = styled('div')`
   }
 `
 
-function DropdownButton({ children, disabled, label, className}) {
-  const [open, setOpen] = useState(false);
-  const container = useRef(null);
+function DropdownButton({ children, disabled, label, className }) {
+  const [open, setOpen] = useState(false)
+  const container = useRef(null)
 
   useEffect(() => {
     const handler = (e) => {
       if (open && container.current && !container.current.contains(e.target)) {
-        setOpen(false);
-      };
-    };
+        setOpen(false)
+      }
+    }
 
-    document.addEventListener('click', handler);
-    return () => document.removeEventListener('click', handler);
+    document.addEventListener('click', handler)
+    return () => document.removeEventListener('click', handler)
   })
 
   function openDropdown(event) {
-    if (disabled) { return; }
+    if (disabled) {
+      return
+    }
 
-    const container = event.currentTarget;
-    setOpen(!open);
-    if(open) {
-      return;
+    const container = event.currentTarget
+    setOpen(!open)
+    if (open) {
+      return
     }
 
     // Need to wait for the browser to render to get menu dimensions
-    setTimeout(() => positionMenu(container));
+    setTimeout(() => positionMenu(container))
   }
 
   /* TODO: Use refs instead of currentTarget and queries.
@@ -47,35 +49,41 @@ function DropdownButton({ children, disabled, label, className}) {
    *       how other libraries do it.
    */
   function positionMenu(container) {
-    const menu = container.querySelector('nav');
+    const menu = container.querySelector('nav')
 
-    const { top, left, bottom, right } = container.getBoundingClientRect();
-    const { width, height } = menu.getBoundingClientRect();
+    const { top, left, bottom, right } = container.getBoundingClientRect()
+    const { width, height } = menu.getBoundingClientRect()
 
-    let newTop = bottom - 5;
+    let newTop = bottom - 5
     let newLeft = right - width + 5
 
     if (newTop + height > document.documentElement.clientHeight) {
-      newTop = top - height;
+      newTop = top - height
     }
 
     if (newLeft + width > document.documentElement.clientWidth) {
-      newLeft = left - width;
+      newLeft = left - width
     }
 
-    menu.style.left = `${newLeft}px`;
-    menu.style.top = `${newTop}px`;
+    menu.style.left = `${newLeft}px`
+    menu.style.top = `${newTop}px`
   }
 
   return (
-    <Container className={className} ref={ container } onClick={ (e) => openDropdown(e) } role="button">
-      <ButtonWithIcon icon={faCaretDown} disabled={disabled}>{label}</ButtonWithIcon>
-      <Dropdown className="dropdown-menu" open={ open } role="navigation">
-        { children }
+    <Container
+      className={className}
+      ref={container}
+      onClick={(e) => openDropdown(e)}
+      role='button'
+    >
+      <ButtonWithIcon icon={faCaretDown} disabled={disabled}>
+        {label}
+      </ButtonWithIcon>
+      <Dropdown className='dropdown-menu' open={open} role='navigation'>
+        {children}
       </Dropdown>
     </Container>
-  );
+  )
 }
 
-export default DropdownButton;
-
+export default DropdownButton
