3c5f0db5c2be7a05e0ef1bb6578ccee724185b67
Cedric Pimenta <cedric.p@vavato.com>
2021-10-21 10:30:08 +0100

    Allow Input to be displayed with error message

--- src/ui/Input.js
+++ src/ui/Input.js
@@ -1,11 +1,18 @@
+import React from 'react'
 import styled from 'styled-components'
 import { rgba } from 'polished'
 
-const Input = styled('input')`
+import { safeProps } from './utils'
+
+const StyledInput = styled('input')`
   border: 2px solid ${(props) => rgba(props.theme.text, 0.4)};
   padding: 8px 16px;
   font-size: 13px;
   border-radius: 3px;
+
+  &.error {
+    border-color: red !important;
+  }
 
   &::placeholder {
     color: ${(props) => rgba(props.theme.text, 0.4)};
@@ -17,5 +24,26 @@ const Input = styled('input')`
     border: 2px solid ${(props) => rgba(props.theme.text, 0.7)};
   }
 `
+
+const ErrorMessage = styled('div')`
+  width: 100%;
+  color: red;
+`
+
+function Input(props) {
+  return (
+    <React.Fragment>
+      <StyledInput
+        {...safeProps(props)}
+        className={props.error ? `${props.className} error` : props.className}
+      />
+      {props.error && props.errorMessage ? (
+        <ErrorMessage>{props.errorMessage}</ErrorMessage>
+      ) : (
+        ''
+      )}
+    </React.Fragment>
+  )
+}
 
 export default Input
